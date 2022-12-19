96768de0f2e2e0d4b7ec71dbee7f7f8f7f0be2d0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2022-12-18 23:59:30 -0600

    WIP

--- mxcurpy.py
+++ mxcurpy.py
@@ -14,18 +14,43 @@ este dato puede ser diferente al oficial.
 
 """
 
-from utilities import get_first_consonant, get_first_vowel, clean_and_format_string
+from utilities import (
+    cleaned_string,
+    get_first_consonant,
+    get_first_vowel,
+    clean_and_format_string,
+)
 
 
 def _generate_first_part(names, lastname, second_lastname=""):
 
     # Puede que algunos usuarios no tengan segundo apellido, y se tiene que usar una X
-    # Ponemos la "x" para extraerla como primera letra.
+    # Ponemos la "x" para extraerla como primera letra
+
     if not second_lastname:
         second_lastname = "x"
     return clean_and_format_string(
         f"{lastname[0]}{get_first_vowel(lastname)}{second_lastname[0]}{names[0]}"
     )
+
+
+def _generate_numeric_part(birth_date):
+    """Recibe la fecha de nacimiento como cadena y la transforma en la
+    parte numérica del CURP o RFC (en este caso son idénticas)"""
+    birth_date = birth_date.strip()
+    parts = birth_date.split("-")
+    day, month, year = parts[0], parts[1], parts[2]
+    if len(day) < 2:
+        day = "0" + day
+    if len(month) < 2:
+        month = "0" + month
+    return f"{year[2:]}{month}{day}"
+
+
+def _generate_common_part(names, lastname, second_lastname="", birth_date=""):
+    """Genera la parte común del CURP y del RFC es decir, la compuesta por el nombre
+    completo y la fecha de nacimiento"""
+    return f"{_generate_first_part(names, lastname, second_lastname)}{_generate_numeric_part(birth_date)}"
 
 
 def curp(
@@ -35,7 +60,7 @@ def curp(
     brith_date: str,
     birth_state: str,
     sex: str,
-) -> str:
+):
     """Devuelve el CURP bien formado usando los datos básicos.
     Nota: el dígito verificador no es correcto porque es asignado al momento de
     creación y registro del curp por una entidad nacional.
@@ -60,4 +85,7 @@ def curp(
     bool
         True if successful, False otherwise.
     """
-    pass
+    first_half = _generate_common_part(names, lastname, second_lastname, birth_date)
+    if sex not in ("h", "H", "m", "n"):
+        raise "Sex formatting is incorrect, must be an 'h' form men or a 'm' for women"
+    second_half = f"{sex}"
--- tests/mxcurpy_test.py
+++ tests/mxcurpy_test.py
@@ -5,7 +5,7 @@ from utilities import (
     get_first_consonant,
     get_first_vowel,
 )
-from mxcurpy import _generate_first_part
+from mxcurpy import _generate_first_part, _generate_numeric_part, _generate_common_part
 
 
 @pytest.mark.parametrize("t_input,expected", (("á", "a"), ("é", "e")))
@@ -57,3 +57,29 @@ def test_extract_first_vowel(t_input, expected):
 def test_generate_first_part(t_input, expected):
     names, lastname, second_lastname = t_input
     assert _generate_first_part(names, lastname, second_lastname) == expected
+
+
+@pytest.mark.parametrize(
+    "t_input,expected",
+    (
+        ("12-08-1989", "890812"),
+        ("25-11-1988", "881125"),
+        ("05-05-1986", "860505"),
+    ),
+)
+def test_generate_numeric_part(t_input, expected):
+    assert _generate_numeric_part(t_input) == expected
+
+
+@pytest.mark.parametrize(
+    "t_input,expected",
+    (
+        (("Héctor Iván", "Patricio", "Moreno", "12-08-1989"), "pamh890812"),
+        (("Armando", "Palermo", "Torres", "12-12-1936"), "pata361212"),
+    ),
+)
+def test_generate_common_part(t_input, expected):
+    names, lastname, second_lastname, birth_date = t_input
+    assert (
+        _generate_common_part(names, lastname, second_lastname, birth_date) == expected
+    )
